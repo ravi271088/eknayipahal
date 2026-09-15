e7d25a5de2f9cff619aa9aa5ffe3172fa324e8f5
ravi271088 <ravi.in.touch85@gmail.com>
2026-09-15 22:10:54 +0530

    first commit

--- main.go
+++ main.go
@@ -1,30 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"text/template"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Starting server on :8080")
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*.html")
+
 	r.Static("/static", "./static")
 
-	// Home route
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "home", gin.H{"title": "Home - Ek Nayi Pahal NGO"})
+		tmpl := template.Must(template.ParseFiles(
+			"templates/layout.html",
+			"templates/home.html",
+		))
+
+		tmpl.ExecuteTemplate(c.Writer, "home", gin.H{
+			"title": "Home - Ek Nayi Pahal NGO",
+		})
 	})
 
-	// About route
 	r.GET("/about", func(c *gin.Context) {
-		c.HTML(200, "about", gin.H{"title": "About Us - Ek Nayi Pahal NGO"})
+		tmpl := template.Must(template.ParseFiles(
+			"templates/layout.html",
+			"templates/about.html",
+		))
+
+		tmpl.ExecuteTemplate(c.Writer, "about", gin.H{
+			"title": "About Us - Ek Nayi Pahal NGO",
+		})
 	})
 
-	// Contact route
 	r.GET("/contact", func(c *gin.Context) {
-		c.HTML(200, "contact", gin.H{"title": "Contact Us - Ek Nayi Pahal NGO"})
+		tmpl := template.Must(template.ParseFiles(
+			"templates/layout.html",
+			"templates/contact.html",
+		))
+		tmpl.ExecuteTemplate(c.Writer, "contact", gin.H{
+			"title": "Contact Us - Ek Nayi Pahal NGO",
+		})
 	})
 
 	r.Run(":8080")
